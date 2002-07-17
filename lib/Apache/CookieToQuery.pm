553a75a74f20087fab4f90d9b120eb63abe51fdb
Alex Pavlovic <alexp@cpan.org>
2002-07-17 00:09:59 +0000

    Import of ALEXP/Apache-CookieToQuery-1.03 from CPAN.
    gitpan-cpan-distribution: Apache-CookieToQuery
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         ALEXP/Apache-CookieToQuery-1.03.tar.gz
    gitpan-cpan-author:       ALEXP
    gitpan-cpan-maturity:     released

--- lib/Apache/CookieToQuery.pm
+++ lib/Apache/CookieToQuery.pm
@@ -4,7 +4,7 @@ use strict;
 BEGIN {
 	use Exporter ();
 	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION     = 1.02;
+	$VERSION     = 1.03;
 	@ISA         = qw (Exporter);
 	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw ();
